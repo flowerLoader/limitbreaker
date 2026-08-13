2b81b3e12fc276fa1c17aca632b3e3e98c5effbc
Damon Blais <damon.blais@gmail.com>
2026-08-13 12:58:28 -0700

    style: apply Biome 2.5.8 formatting and autofixes

--- flowerteam.limitbreaker.ts
+++ flowerteam.limitbreaker.ts
@@ -2,29 +2,30 @@
  * A simple patch to expose the game's limits and allow tweaking them
  */
 
-import { FlowerMeta, FlowerAPI, LogSource, BasePlugin } from "@flowerloader/api";
-import { GameDataCOAW } from "@flowerloader/coawtypes";
+import {
+	BasePlugin,
+	FlowerAPI,
+	type FlowerMeta,
+	LogSource,
+} from "@flowerloader/api";
+import type { GameDataCOAW } from "@flowerloader/coawtypes";
 
-export const META: FlowerMeta =
-{
-    GUID: "flowerteam.limitbreaker",
-    VERSION: "1.0.1",
-    NAME: "Limit Breaker",
-    ENABLED: true
+export const META: FlowerMeta = {
+	GUID: "flowerteam.limitbreaker",
+	VERSION: "1.0.1",
+	NAME: "Limit Breaker",
+	ENABLED: true,
 };
 
-export default class Plugin extends BasePlugin<GameDataCOAW>
-{
-    CONFIG =
-        {
-            //Default: 5
-            MaxPartySize: 10,
-        }
+export default class Plugin extends BasePlugin<GameDataCOAW> {
+	CONFIG = {
+		//Default: 5
+		MaxPartySize: 10,
+	};
 
-    Awake()
-    {
-        this.logger.write("Setting limits");
-        this.flower.GetGameMain().tGameMain.tGameCharactor.playerTeamMemberLimitNum = this.CONFIG.MaxPartySize;
-    }
-
+	Awake() {
+		this.logger.write("Setting limits");
+		this.flower.GetGameMain().tGameMain.tGameCharactor.playerTeamMemberLimitNum =
+			this.CONFIG.MaxPartySize;
+	}
 }
